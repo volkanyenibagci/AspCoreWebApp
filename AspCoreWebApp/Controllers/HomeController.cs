22902b22995ecab4d7ecb50fbca11d036c4d95cb
volkanyenibagci <ku*4uluS>
2021-11-01 01:50:34 +0300

    Kategori commit

--- AspCoreWebApp/Controllers/HomeController.cs
+++ AspCoreWebApp/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using DevExtreme;
 namespace AspCoreWebApp.Controllers
 {
     public class HomeController : Controller
@@ -30,6 +31,53 @@ namespace AspCoreWebApp.Controllers
             var DepartmanSayisi=_context.Departmans.Count();
             ViewBag.DepartmanSayisi = DepartmanSayisi;
             return View();
+        }
+        
+        public ActionResult Kategori()
+        {
+            
+            var degerler = _context.Kategoris.ToList();
+            //IQueryable<Kategori> query = db.Kategoris;
+
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            _context.Kategoris.Add(k);
+            _context.SaveChanges();
+            return RedirectToAction("Kategori");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = _context.Kategoris.Find(id);
+            _context.Kategoris.Remove(ktg);
+            _context.SaveChanges();
+            return RedirectToAction("Kategori");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = _context.Kategoris.Find(id);
+            return View("KategoriGetir",kategori);
+
+        }
+
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktgr = _context.Kategoris.Find(k.KategoriID);
+            ktgr.KategoriAd = k.KategoriAd;
+            _context.SaveChanges();
+            return RedirectToAction("Kategori");
+
         }
 
         public IActionResult Privacy()
